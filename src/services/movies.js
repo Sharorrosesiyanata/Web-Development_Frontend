b6ba6022ce23129ce09634f4b1f3af50738087e4
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2025-01-17 10:12:50 +0200

    commit

--- src/services/movies.js
+++ src/services/movies.js
@@ -2,35 +2,35 @@ import axios from "axios";
 
 class MovieDataService {
   getAll(page = 0) {
-    return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
+    return axios.get(`https://web-development-backend-bscy.onrender.com/api/v1/movies?page=${page}`);
   }
 
   get(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+    return axios.get(`https://web-development-backend-bscy.onrender.com/api/v1/movies/id/${id}`);
   }
 
   find(query, by = "title", page = 0, rating) {
     return axios.get(
-      `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+      `https://web-development-backend-bscy.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
     );
   }
 
   createReview(data) {
-    return axios.post("http://localhost:5000/api/v1/movies/review", data);
+    return axios.post("https://web-development-backend-bscy.onrender.com/api/v1/movies/review", data);
   }
 
   updateReview(data) {
-    return axios.put("http://localhost:5000/api/v1/movies/review", data);
+    return axios.put("https://web-development-backend-bscy.onrender.com/api/v1/movies/review", data);
   }
 
   deleteReview(id, userId) {
-    return axios.delete("http://localhost:5000/api/v1/movies/review", {
+    return axios.delete("https://web-development-backend-bscy.onrender.com/api/v1/movies/review", {
       data: { review_id: id, user_id: userId },
     });
   }
 
   getRatings() {
-    return axios.get("http://localhost:5000/api/v1/movies/ratings");
+    return axios.get("https://web-development-backend-bscy.onrender.com/api/v1/movies/ratings");
   }
 }
 
